67349a4421604dcb07470043f2dddf55d677fed5
asaporta11 <arielle.saporta@gmail.com>
2016-08-04 19:54:53 -0500

    more server integrated blargh

---
+++ empirical/local_settings.py
@@ -0,0 +1,9 @@
+# These are for variables that should not be in GitHub
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'databasesecretkeygoeshere'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS=['*']
